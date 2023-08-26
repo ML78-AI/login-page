663af86d0a9ec82ef80a0df2e7979ca82ce8c055
Mathieu Laizé <mathieulaize78@orange.fr>
2023-08-26 17:13:25 +0200

    Entrainement avec opacity et text-emphasis

--- src/screens/DashboardScreen.jsx
+++ src/screens/DashboardScreen.jsx
@@ -1,5 +1,12 @@
+import classes from "./bloc.module.css";
+
 function DashboardScreen() {
-    return <h1>Connexion réussie</h1>;
+    return (
+        <>
+            <h1 className={classes["title"]}>Connexion réussie</h1>
+            <div className={classes["bloc"]}></div>
+        </>
+    );
 }
 
 export default DashboardScreen;
